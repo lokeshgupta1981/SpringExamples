497b84d6eb3c2b7bc355476bb09ed1d44e59d1c7
howtodoinjava <howtodoinjava@gmail.com>
2018-05-05 13:34:54 +0530

    Which Main Class to Use

---
+++ main-class/src/main/java/com/springexamples/demo/MainClassOne.java
@@ -0,0 +1,13 @@
+package com.springexamples.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainClassOne 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(MainClassOne.class, args);
+    }
+}
---
+++ main-class/src/main/java/com/springexamples/demo/MainClassTwo.java
@@ -0,0 +1,13 @@
+package com.springexamples.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainClassTwo
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(MainClassTwo.class, args);
+    }
+}
