dd2a4dd0fe63a6713676506ade6b67a02d9b218c
howtodoinjava <howtodoinjava@gmail.com>
2018-05-05 14:15:23 +0530

    Get all loaded bean info examples

---
+++ get-loaded-beans-info/src/main/java/com/springexamples/demo/Application.java
@@ -0,0 +1,27 @@
+package com.springexamples.demo;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+
+	}
+
+	@Bean
+	public CommandLineRunner run(ApplicationContext appContext) {
+		return args -> {
+
+			String[] beans = appContext.getBeanDefinitionNames();
+			Arrays.stream(beans).sorted().forEach(System.out::println);
+
+		};
+	}
+}
