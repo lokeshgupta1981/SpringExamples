b6251e1bb0218cd6670b2c738c867ced6f11e977
howtodoinjava <howtodoinjava@gmail.com>
2018-05-05 23:01:53 +0530

    More Examples

---
+++ ssl-https/src/main/java/com/springexamples/demo/config/SSLConfig.java
@@ -0,0 +1,56 @@
+package com.springexamples.demo.config;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+public class SSLConfig {
+	
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(redirectConnector());
+		tomcat.addAdditionalTomcatConnectors(createSslConnector());
+		return tomcat;
+	}
+
+	private Connector createSslConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+		try {
+			File keystore = new ClassPathResource("keystore.jks").getFile();
+			File truststore = new ClassPathResource("keystore.jks").getFile();
+			connector.setScheme("https");
+			connector.setSecure(true);
+			connector.setPort(8443);
+			protocol.setSSLEnabled(true);
+			protocol.setKeystoreFile(keystore.getAbsolutePath());
+			protocol.setKeystorePass("password");
+			protocol.setTruststoreFile(truststore.getAbsolutePath());
+			protocol.setTruststorePass("password");
+			protocol.setKeyAlias("tomcat");
+			return connector;
+		} catch (IOException ex) {
+			throw new IllegalStateException(
+					"can't access keystore: [" + "keystore" + "] or truststore: [" + "keystore" + "]", ex);
+		}
+	}
+
+	private Connector redirectConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+
+		return connector;
+	}
+}
