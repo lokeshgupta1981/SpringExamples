aa2be7ed7e8cc4604eef0e0000f7d4e9e2ff0a96
Lokesh Gupta <howtodoinjava@gmail.com>
2019-09-09 23:47:15 +0530

    Updated to spring boot 2.1.8

--- server-customization/src/main/java/com/springexamples/demo/App.java
+++ server-customization/src/main/java/com/springexamples/demo/App.java
@@ -2,12 +2,12 @@ package com.springexamples.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+/*import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatus;*/
 
 @SpringBootApplication
 public class App 
