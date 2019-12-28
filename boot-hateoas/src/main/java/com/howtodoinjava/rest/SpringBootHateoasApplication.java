636945b59c50e0f82e6ae004b74bbc992d2b2580
Lokesh Gupta <howtodoinjava@gmail.com>
2019-12-29 02:54:54 +0530

    Update SpringBootHateoasApplication.java

--- boot-hateoas/src/main/java/com/howtodoinjava/rest/SpringBootHateoasApplication.java
+++ boot-hateoas/src/main/java/com/howtodoinjava/rest/SpringBootHateoasApplication.java
@@ -3,6 +3,8 @@ package com.howtodoinjava.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @SpringBootApplication
 //@EnableAutoConfiguration(exclude = HypermediaAutoConfiguration.class)
@@ -10,5 +12,10 @@ public class SpringBootHateoasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHateoasApplication.class, args);
+	}
+	
+	@Bean
+	ForwardedHeaderFilter forwardedHeaderFilter() {
+	    return new ForwardedHeaderFilter();
 	}
 }
